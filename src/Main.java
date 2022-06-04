6397f32121000303ed2c6b24a4f213e205db9e22
itsthooor <71508362+itsthooor@users.noreply.github.com>
2022-06-04 20:23:04 +0200

    Added Main.java file of the game

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean numberGuessedCorrectly = false;
+        int numberToGuess = createNumberToGuess();
+        int guessTries = 0;
+
+        System.out.println("Welcome to the random number guesser minigame.\n" + "You need to guess a number between 0 and 100,\n" + "while trying to use the lowest amount of guesses overall.");
+
+        while (!numberGuessedCorrectly) {
+            System.out.println("Guess the number:");
+            Integer guessedNumber = sc.nextInt();
+            String guessCheck = numberGuessChecker(guessedNumber, numberToGuess);
+            guessTries += 1;
+
+            switch (guessCheck) {
+                case "correct":
+                    numberGuessedCorrectly = true;
+                    break;
+                case "lower":
+                    System.out.println("The number to guess is lower...");
+                    break;
+                case "higher":
+                    System.out.println("The number to guess is higher...");
+                    break;
+            }
+        }
+
+        System.out.println("The number to guess was " + numberToGuess + "!\nCongrats on the win in " + guessTries + " tries!");
+    }
+
+    private static int createNumberToGuess() {
+        Random rand = new Random();
+
+        return rand.nextInt(100);
+    }
+
+    private static String numberGuessChecker(Integer numberGuessed, Integer numberToGuess) {
+        String returnMessage;
+
+        if (Objects.equals(numberGuessed, numberToGuess)) {
+            returnMessage = "correct";
+        } else if (numberGuessed > numberToGuess) {
+            returnMessage = "lower";
+        } else {
+            returnMessage = "higher";
+        }
+
+        return returnMessage;
+    }
+}
